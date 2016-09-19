3880a487bf0efe40601c145a801452e404170a59
Sedohr <nsonos@gmail.com>
2016-09-18 20:28:53 -0500

    Input/export updates
    Coming together more now.
    -Changed it to force users to set new password upon login.
    -Exports passwords for all users to csv file isntead of just the last
    user.
    -Pulls from the same directory of the script instead of needing to
    specify the location.
    -Prompts for the csv filename.

--- useradmincsv.ps1
+++ useradmincsv.ps1
@@ -11,10 +11,12 @@ $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri ht
 #Imports the new Exchange session into the current PowerShell session"
 Import-PSSession $Session
 
+# Prompts user for CSV file name
+$csv = Read-Host -Prompt 'Insert CSV file name (without .csv)'
 # Imports the CSV file provided in the directory and makes the user variables
-$users = Import-Csv C:\Users\scot5644\Desktop\import1.csv
-# Runs through users imported on csv and creates their office365 user
-$users | ForEach-Object { New-MsolUser -UserPrincipalName $_."User Name" -FirstName $_."First Name" -LastName $_."Last Name" -DisplayName $_."Display Name" -Title $_."Job Title" -Department $_."Department" -Office $_."Office Number" -PhoneNumber $_."Office Phone" -MobilePhone $_."Mobile Phone" -Fax $_."Fax" -StreetAddress $_."Address" -City $_."City" -State $_."State or Province" -PostalCode $_."State or Province" -Country $_."Country or Region" | select UserPrincipalName,Password }
+$users = Import-Csv -Path "$csv.csv"
+# Runs through users imported on csv and creates their office365 user. Then it exports the passwords to a csv file
+$users | ForEach-Object { New-MsolUser -UserPrincipalName $_."User Name" -FirstName $_."First Name" -LastName $_."Last Name" -DisplayName $_."Display Name" -Title $_."Job Title" -Department $_."Department" -Office $_."Office Number" -PhoneNumber $_."Office Phone" -MobilePhone $_."Mobile Phone" -Fax $_."Fax" -StreetAddress $_."Address" -City $_."City" -State $_."State or Province" -PostalCode $_."State or Province" -Country $_."Country or Region" -ForceChangePassword $True | select UserPrincipalName,Password | Export-Csv -Append -Path passwords.csv }
 # Assigns Password admin role for users imported
 $users | ForEach-Object { Add-MsolRoleMember -RoleName "Helpdesk Administrator" -RoleMemberEmailAddress $_."User Name" }
 
